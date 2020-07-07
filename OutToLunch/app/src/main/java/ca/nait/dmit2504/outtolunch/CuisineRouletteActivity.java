4334ee1d72f1ee44c87a51707452ef59a0c9843e
irenaliu <irena@ualberta.ca>
2020-07-06 19:29:44 -0600

    Successfully calculate cuisine based on animation end

--- OutToLunch/app/src/main/java/ca/nait/dmit2504/outtolunch/CuisineRouletteActivity.java
+++ OutToLunch/app/src/main/java/ca/nait/dmit2504/outtolunch/CuisineRouletteActivity.java
@@ -21,8 +21,14 @@ public class CuisineRouletteActivity extends AppCompatActivity implements Animat
     private Button mFindBtn;
     private boolean mBtnRotation = true;
     private int mNum = 7;
-    private long mDegrees = 0;
+    private int mDegrees = 0, mDegreesOld = 0;
+    private Random mRandom;
+    //7 sectors on the wheel = 51.42857 degrees each
+    //1st sector starts from half the previous sector, 51.42857/ 2 = 25.714285, ends in 25.714285 + 51.42857 = 77.1429
+    // 77.1429 is 3x 25.714285, so 25.714285 = factor
+    private static final float FACTOR = 25.714285f; //use this to calculate sectors
     private static final String[] mCuisines = {"Chinese", "Vegetarian", "Indian", "Japanese", "Viet", "American", "Italian"};
+    private String selectedCuisine = "";
 
 
     private ImageView mWheelImgView;
@@ -41,10 +47,12 @@ public class CuisineRouletteActivity extends AppCompatActivity implements Animat
 
         mWheelImgView.setOnClickListener(v -> {
             //create random instance
-            int random = new Random().nextInt(360) + 3600;
-            RotateAnimation rotateAnimation = new RotateAnimation((float) mDegrees, (float) (mDegrees + ((long) random)),1, 0.5f, 1, 0.5f);
-            mDegrees = mDegrees + (long)random % 360;
-            rotateAnimation.setDuration((long) random);
+            mRandom = new Random();
+            mDegreesOld = mDegrees % 360;
+            mDegrees = mRandom.nextInt(360) + 3600;
+            RotateAnimation rotateAnimation = new RotateAnimation(mDegreesOld, mDegrees,RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+
+            rotateAnimation.setDuration(3600);
             rotateAnimation.setFillAfter(true);
             rotateAnimation.setInterpolator(new DecelerateInterpolator());
             rotateAnimation.setAnimationListener(this);
@@ -70,9 +78,8 @@ public class CuisineRouletteActivity extends AppCompatActivity implements Animat
 
     @Override
     public void onAnimationEnd(Animation animation) {
-        int selected = ((int)(((double)mNum) - Math.floor(((double)mDegrees) / (360.0d / ((double)mNum)))));
-
-        Toast toast = Toast.makeText(this, String.valueOf(selected), Toast.LENGTH_SHORT);
+        selectedCuisine = currentCuisine(360 - (mDegrees % 360));
+        Toast toast = Toast.makeText(this, selectedCuisine, Toast.LENGTH_SHORT);
         toast.setGravity(49, 0,0);
         toast.show();
         mBtnRotation = true;
@@ -81,5 +88,28 @@ public class CuisineRouletteActivity extends AppCompatActivity implements Animat
     @Override
     public void onAnimationRepeat(Animation animation) {
 
+    }
+
+    private String currentCuisine(int degrees) {
+        String cuisine = "Chinese";
+        if (degrees >= (FACTOR * 1) && degrees < (FACTOR * 3)) {
+            cuisine = "Italian";
+        }
+        if (degrees >= (FACTOR * 3) && degrees < (FACTOR * 5)) {
+            cuisine = "American";
+        }
+        if (degrees >= (FACTOR * 5) && degrees < (FACTOR * 7)) {
+            cuisine = "Vietnamese";
+        }
+        if (degrees >= (FACTOR * 7) && degrees < (FACTOR * 9)) {
+            cuisine = "Japanese";
+        }
+        if (degrees >= (FACTOR * 9) && degrees < (FACTOR * 11)) {
+            cuisine = "Indian";
+        }
+        if (degrees >= (FACTOR * 11) && degrees < (FACTOR * 13)) {
+            cuisine = "Vegetarian";
+        }
+        return cuisine;
     }
 }
