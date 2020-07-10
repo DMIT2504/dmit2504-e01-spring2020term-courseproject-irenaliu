c8128846a881417473f4119481c3d1c782dd60b9
irenaliu <irena@ualberta.ca>
2020-07-10 14:42:59 -0600

    Abandon FoodLookup project, api not working

--- Mini-Projects/FoodLookup/app/src/main/java/ca/nait/dmit2504/foodlookup/DetailsFragment.java
+++
@@ -1,4 +0,0 @@
-package ca.nait.dmit2504.foodlookup;
-
-public class DetailsFragment extends BaseFragment {
-}
--- Mini-Projects/FoodLookup/app/src/main/java/ca/nait/dmit2504/foodlookup/NutritionFragment.java
+++
@@ -1,4 +0,0 @@
-package ca.nait.dmit2504.foodlookup;
-
-public class NutritionFragment extends BaseFragment {
-}
